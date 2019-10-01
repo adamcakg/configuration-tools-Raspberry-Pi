3dc72df5e890928f907fc2fabb1ea3dafc21ab59
Gabriel Adamčák <gabriel.adamcak@student.tuke.sk>
2019-10-01 19:52:17 +0200

    Initial commit for new version of a project, we're going to use GTK3 instead of tkinter

--- all_set.py
+++
@@ -1,9 +0,0 @@
-import tkinter
-from page import Page
-
-
-class AllSetPage(Page):
-    def __init__(self, *args, **kwargs):
-        Page.__init__(self, *args, **kwargs)
-        tkinter.Label(self, text="ALL SET", font=("Courier", 44)).place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
-        tkinter.Button(self, text="Start using PI", command=self.quit).place(relx=0.5, rely=0.8, anchor=tkinter.CENTER)
--- page.py
+++
@@ -1,9 +0,0 @@
-import tkinter
-
-
-class Page(tkinter.Frame):
-    def __init__(self, *args, **kwargs):
-        tkinter.Frame.__init__(self, *args, **kwargs)
-
-    def show(self):
-        self.lift()
