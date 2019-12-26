fdc75d5140307f18caec853f828f44e2a77cc803
Gabriel Adamčák <gabriel.adamcak@student.tuke.sk>
2019-12-26 11:20:20 -0500

    Adding WiFi refresh button if user wants to refresh networks, refactor at password page as well as WiFi page

--- first_use/wifipage/__init__.py
+++ first_use/wifipage/__init__.py
@@ -2,20 +2,21 @@ import update_page as update_page
 import passwordpage as password_page
 from .handler import Handler
 
-from wifi import Cell  
+ 
 
 import gi
 
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, GLib
+from gi.repository import Gtk
 
 
 class WifiPage:
     def __init__(self):
         self.__builder = Gtk.Builder()                  # Initializing builder
         self.__builder.add_from_file('wifipage/wifi.glade')   # creating object from XML(.glade files)
-        self.search()
-
+        self.handler = Handler(builder=self.__builder)
+        
+        self.handler.search()
 
 # METHOD TO GO NEXT
 # ----------------------------------------------------------------------------------------------------------------------
@@ -40,45 +41,13 @@ class WifiPage:
 # METHOD TO CONNECT HANDLER
 # ----------------------------------------------------------------------------------------------------------------------
     def connect_handler(self, controller):
-        self.__builder.connect_signals(Handler(controller))
+        self.handler.add_controller(controller)
+        self.__builder.connect_signals(self.handler)
 
 # METHOD TO EXECUTE PAGE
 # ----------------------------------------------------------------------------------------------------------------------
     def execute(self):
-        pass
+        self.handler.search()
     
-    def search(self):
-        available_networks = Cell.all('wlan0')
-        print(available_networks)
-        list_of_networks = []
-        for item in available_networks:
-            list_of_networks.append(item)
-            
-        wifi_tree = self.__builder.get_object('wifi_tree')
-        
-        store = Gtk.ListStore(str, bool, int)
-        list_of_networks.sort(key=lambda x: x.signal)
-        list_of_networks.reverse()
-        for item in list_of_networks:
-            store.append([item.ssid[:20], item.encrypted, item.signal])
-        
-        wifi_tree.set_model(store)
-        
-        column = Gtk.TreeViewColumn("Network")
 
-        ssid = Gtk.CellRendererText()
-        encrypted = Gtk.CellRendererText()
-        signal = Gtk.CellRendererText()
-
-        column.pack_start(ssid, True)
-        column.pack_start(encrypted, True)
-        column.pack_start(signal, True)
-
-        column.add_attribute(ssid, "text", 0)
-        column.add_attribute(encrypted, "text", 1)
-        column.add_attribute(signal, 'text', 2)
-
-        wifi_tree.append_column(column)
-        
-        
         
--- first_use/wifipage/handler.py
+++ first_use/wifipage/handler.py
@@ -1,17 +1,73 @@
+from wifi import Cell
 
+
+import gi
+
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
 
 class Handler:
-    def __init__(self, controller):
+    def __init__(self, builder):
+        self.builder = builder
+        
+    def add_controller(self, controller):
         self.controller = controller
 
 # METHOD TO GO NEXT
 # ----------------------------------------------------------------------------------------------------------------------
     def next(self, button):
-        self.controller.next()
+        self.controller.execute()
+        #self.controller.next()
 
 # METHOD TO GO BACK
 # ----------------------------------------------------------------------------------------------------------------------
     def back(self, button):
         self.controller.back()
-    
+        
+        
+        
+        
+        
+        
+    def button_pressed(self, button):
+        self.search()
+        
+        
+    def search(self):
+        available_networks = Cell.all('wlan0')
+        print(available_networks)
+        list_of_networks = []
+        for item in available_networks:
+            list_of_networks.append(item)
+            
+        wifi_tree = self.builder.get_object('wifi_tree')
+        
+        columns_to_remove = wifi_tree.get_columns()         #REMOVING COLUMNS IF EXIST ALREADY
+        if columns_to_remove is not None:
+            for column in columns_to_remove:
+                wifi_tree.remove_column(column)
+        
+        store = Gtk.ListStore(str, bool, int)
+        list_of_networks.sort(key=lambda x: x.signal)
+        list_of_networks.reverse()
+        for item in list_of_networks:
+            store.append([item.ssid[:20], item.encrypted, item.signal])
+        
+        wifi_tree.set_model(store)
+        
+        column = Gtk.TreeViewColumn("Network")
 
+        ssid = Gtk.CellRendererText()
+        encrypted = Gtk.CellRendererText()
+        signal = Gtk.CellRendererText()
+
+        column.pack_start(ssid, True)
+        column.pack_start(encrypted, True)
+        column.pack_start(signal, True)
+
+        column.add_attribute(ssid, "text", 0)
+        column.add_attribute(encrypted, "text", 1)
+        column.add_attribute(signal, 'text', 2)
+
+        wifi_tree.append_column(column)
+        
