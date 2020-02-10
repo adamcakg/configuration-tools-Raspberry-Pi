b46c80796574bb559569de93e1286815d1eb6300
Gabriel Adamčák <gabriel.adamcak@student.tuke.sk>
2020-02-10 20:02:17 +0100

    Refactor

---
+++ settings/interfaces/handler.py
@@ -0,0 +1,8 @@
+class Handler:
+    def __init__(self, builder, controller=None):
+        self.builder = builder
+
+    # ADDING CONTROLLER TO HANDLER
+    # ----------------------------------------------------------------------------------------------------------------------
+    def add_controller(self, controller):
+        self.controller = controller
---
+++ settings/localisation/__init__.py
@@ -0,0 +1,33 @@
+import gi
+from .handler import Handler
+from page import Page
+
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+
+class Localisation(Page):
+    def __init__(self):
+        self.name = 'Localisation'
+        self.__builder = Gtk.Builder()  # Initializing builder
+        self.__builder.add_from_file('localisation/localisation.glade')  # creating object from XML(.glade files)
+
+        self.handler = Handler(builder=self.__builder)
+        self.header = None
+
+    def get_xml_object(self):
+        return self.__builder.get_object('localisation')
+
+    def destroy(self):
+        del self
+
+    def connect_handler(self, controller):
+        self.handler.add_controller(controller)
+        self.__builder.connect_signals(self.handler)
+
+    def connect_header(self, header):
+        self.header = header
+        self.header.set_title(self.name)
+        
+    def get_name(self):
+        return self.name
