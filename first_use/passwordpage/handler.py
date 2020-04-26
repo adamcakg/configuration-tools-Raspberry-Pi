1a4d5a2df68b3024f678cc3ae8c6642b4ba0eede
Gabriel Adamčák <gabriel.adamcak@student.tuke.sk>
2020-04-26 11:11:50 -0400

    card_bg refactor

--- first_use/passwordpage/handler.py
+++ first_use/passwordpage/handler.py
@@ -98,7 +98,9 @@ class Handler:
     def thread_function(self):
         password = keeper['passwordpage']['password'] + '\n' + keeper['passwordpage']['password']
         #os.system('echo "{}" | sudo passwd "pi"'.format(password))
-        os.system('echo "{}" | sudo passwd'.format(password))
+        #os.system('echo "{}" | sudo passwd'.format(password))
+        whoami = os.popen('whoami').read().rstrip()
+        os.system('echo "{}:{}" | sudo chpasswd'.format(whoami, password))
         
     def create_modal(self):
         print('modal function')
