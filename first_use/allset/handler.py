b2d0e5f56cae20a4f21920a8884a66c342571e38
Gabriel Adamčák <gabriel.adamcak@student.tuke.sk>
2020-05-01 16:29:57 -0400

    Added autostart

--- first_use/allset/handler.py
+++ first_use/allset/handler.py
@@ -11,3 +11,8 @@ class Handler:
 
     def back(self,button):
         self.controller.back()
+        
+    def remove_autostart(self):
+        file = os.popen('cat /etc/xdg/lxsession/LXDE-pi/autostart').read().rstrip()
+        file = file.replace('@sudo python3 /etc/first_use','')
+        os.popen('echo "{}" > /etc/xdg/lxsession/LXDE-pi/autostart'.format(file))
